e2fbaec18bfbaa5872ebf7f1280270384fd3049b
tsubaku <adm49@mail.ru>
2018-01-24 17:11:10 +0300

    v0.43

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=ortexsecur.mysql;dbname=ortexsecur_tracking',
-    'username' => 'ortexsecur_mysql',
-    'password' => '2bs6ylg9',
-    'charset' => 'utf8',
-];
